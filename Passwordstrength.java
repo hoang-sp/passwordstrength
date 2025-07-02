8a58d20d5b72efc48d09d389b83933214ff4515c
hoang-sp <chihiro18102005@gmail.com>
2025-07-02 09:54:11 +0700

    Add files via upload

---
+++ Passwordstrength.java
@@ -0,0 +1,44 @@
+package passwordstrength;
+
+import java.util.Scanner; // ✅ Bổ sung import Scanner
+
+public class Passwordstrength {
+
+    public static void main(String[] args) {
+        System.out.println("\n\t\tDataFlair's Password Strength Checker\n");
+        System.out.print("Enter the password: ");
+        
+        Scanner sc = new Scanner(System.in);
+        String password = sc.nextLine();
+        
+        String strength = passwordStrength(password);
+        System.out.println("Strength: " + strength);
+        
+        sc.close(); 
+    }
+
+    public static String passwordStrength(String pass) {
+        boolean containsLowerChar = false, containsUpperChar = false;
+        boolean containsDigit = false, containsSpecialChar = false, minLength = false;
+
+        String special_chars = "!(){}[]:;<>?,@#$%^&*+=_-~`|./'";
+        char[] ch = pass.toCharArray();
+
+        for (char c : ch) {
+            if (Character.isLowerCase(c)) containsLowerChar = true;
+            if (Character.isUpperCase(c)) containsUpperChar = true;
+            if (Character.isDigit(c)) containsDigit = true;
+            if (special_chars.contains(String.valueOf(c))) containsSpecialChar = true;
+        }
+
+        if (pass.length() >= 12) minLength = true;
+
+        if (minLength && containsDigit && containsUpperChar && containsSpecialChar && containsLowerChar) {
+            return "Strong";
+        } else if (minLength && ((containsUpperChar && containsLowerChar) || containsDigit || containsSpecialChar)) {
+            return "Medium";
+        } else {
+            return "Weak";
+        }
+    }
+}
